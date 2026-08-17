3cda4baa24d1c76ffb3231ea69958b2d3634075a
CHELAMPALEM HARI BABU <haribabuai.svpcet@gmail.com>
2026-08-17 18:26:55 +0530

    Initialize Express.js server with routes

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Express.js server is running successfully!");
+});
+
+app.get("/about", (req, res) => {
+    res.send("Welcome to Express.js Lab");
+});
+
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Express API is working",
+        status: "success"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
